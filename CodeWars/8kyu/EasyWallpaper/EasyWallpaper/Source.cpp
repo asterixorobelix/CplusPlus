f619b115d949ef68304515b70c5b17f9a282a886
nathan.stasin <nathanstasin@gmail.com>
2017-08-28 09:42:02 +0200

    Started EasyWallpaper kata

---
+++ CodeWars/8kyu/EasyWallpaper/EasyWallpaper/Source.cpp
@@ -0,0 +1,27 @@
+/*
+John wants to decorate a room with wallpaper. He has been told that making sure he has the right amount of wallpaper is more complex than it sounds. 
+John knows that the rectangular room has a length of l meters, a width of w meters, a height of h meters. The standard width of the rolls he wants to buy is 52 centimeters. 
+The length of a roll is 10 meters. It’s best to have an extra length of wallpaper handy in case of mistakes, so he wants to buy a length 15% greater than the one he needs.
+
+Your function wallpaper(l, w, h) should return as a plain English word in lower case the number of rolls he must buy.
+#Example: wallpaper(4, 3.5, 3) should return "ten"
+
+#Notes:
+
+0 <= l, w, h (floating numbers), it can happens that w x h x l is zero
+the integer r (number of rolls) will always be less or equal to 20
+*/
+#include <string>
+#include <iostream>
+
+void wallpaper(double l, double w, double h);
+
+int main() {
+
+}
+
+void wallpaper(double l, double w, double h) {
+	double ROLLWIDTH = 0.52; //m
+	double ROLLLENGTH = 0.1; //m
+	double MISTAKES = 0.15;  //m
+}
