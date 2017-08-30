d49c890d3be2f1e820a1a0cfb37f937140655bca
nathan.stasin <nathanstasin@gmail.com>
2017-08-30 17:07:05 +0200

    Started: puzzle/power-of-thor-episode-1

--- CodingGame/CodingGame/Source.cpp
+++ CodingGame/CodingGame/Source.cpp
@@ -6,36 +6,29 @@
 using namespace std;
 
 /**
- * The while loop represents the game.
- * Each iteration represents a turn of the game
- * where you are given inputs (the heights of the mountains)
- * and where you have to print an output (the index of the mountain to fire on)
- * The inputs you are given are automatically updated according to your last actions.
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ * ---
+ * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
  **/
 int main()
 {
+    int lightX; // the X position of the light of power
+    int lightY; // the Y position of the light of power
+    int initialTX; // Thor's starting X position
+    int initialTY; // Thor's starting Y position
+    cin >> lightX >> lightY >> initialTX >> initialTY; cin.ignore();
 
     // game loop
     while (1) {
-        
-		vector<int> Heights;
+        int remainingTurns; // The remaining amount of turns Thor can move. Do not remove this line.
+        cin >> remainingTurns; cin.ignore();
 
-        for (int i = 0; i < 8; i++) {// i represents the mountain
-            int mountainH; // represents the height of one mountain.
-            cin >> mountainH; cin.ignore();
-            //cerr<<mountainH<<endl;
-            Heights.push_back(mountainH);
-        }
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
 
-		int GreatestHeight{ 0 }, MountainNum;
 
-		for (int i = 0; i < Heights.size(); i++) {
-			if (Heights[i] > GreatestHeight) {
-				GreatestHeight = Heights[i];
-				MountainNum = i;
-			}
-		}
-
-        cout << MountainNum << endl; // The index of the mountain to fire on.
+        // A single line providing the move to be made: N NE E SE S SW W or NW
+        cout << "SE" << endl;
     }
 }
