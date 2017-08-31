5977f59845d158c13112c90096e7543c49c7ad44
nathan.stasin <nathanstasin@gmail.com>
2017-08-31 08:52:47 +0200

    Initial commit

---
+++ CodingGame/Puzzles/ClassicPuzzle-Easy/ASCIIart/ASCIIart/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+    int L;
+    cin >> L; cin.ignore();
+    int H;
+    cin >> H; cin.ignore();
+    string T;
+    getline(cin, T);
+    for (int i = 0; i < H; i++) {
+        string ROW;
+        getline(cin, ROW);
+    }
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "answer" << endl;
+}
