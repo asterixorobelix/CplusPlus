6b5f04e1cc8195902b62e550e425f2b6372aa113
nathan.stasin <nathanstasin@gmail.com>
2017-08-31 15:22:06 +0200

    Initial Commit

---
+++ ProblemSolvingWithC++Savitch-Mock/Chapter3/PP2/PP2/Source.cpp
@@ -0,0 +1,8 @@
+/*
+chapter3, page 204, programming projects 2
+A triangle can be of three types: all sides are equal, only two sides are equal, all three sides are unequal.
+Write a program which allows the user to enter the three sides of the triangle. Use three double variables to store the triangle sides.
+The program should print the three angles if the triangle is equilateral.
+If the triangle is isosceles, it should prompt the user to input the odd angle and the program should print the remaining angles.
+If all the sides are unequal, the program should do nothing.
+*/
