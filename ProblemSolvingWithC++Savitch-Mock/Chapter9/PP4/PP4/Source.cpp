649a6aead663949d245cceaefe766bd295a013ec
nathan.stasin <nathanstasin@gmail.com>
2017-09-01 08:19:41 +0200

    Initial Commit

---
+++ ProblemSolvingWithC++Savitch-Mock/Chapter9/PP4/PP4/Source.cpp
@@ -0,0 +1,11 @@
+/*
+Programming Project 4, Savitch, Chapter 9, pg 565
+A palindrome word reads the same both backward and forward. Write a function which takes a string as an input parameter.
+The program should then test whether the string is a palindrome.
+One pointer should point to the beginning of the word and another which points to the end of the word.
+If the two words in these pointers is the same, then print palindrome; otherwise print non-palindrome.
+*/
+
+int main() {
+
+}
