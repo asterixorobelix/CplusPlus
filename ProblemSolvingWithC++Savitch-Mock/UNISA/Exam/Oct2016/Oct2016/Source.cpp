bfc856c9d1efbcd54ea07f85457f88493fc45591
nathan.stasin <nathanstasin@gmail.com>
2017-09-11 09:31:05 +0200

    asdf

--- ProblemSolvingWithC++Savitch-Mock/UNISA/Exam/Oct2016/Oct2016/Source.cpp
+++ ProblemSolvingWithC++Savitch-Mock/UNISA/Exam/Oct2016/Oct2016/Source.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void Q1point1(int x, int y);
 
+int main() {
+	Q1point1(15, 10);
+}
+
+void Q1point1(int x, int y) {
+	cout << "Q1.1a" << endl;
+	int res = x >= y - x;
+
+	cout << res << endl;
 }
